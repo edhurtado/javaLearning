7a23cee0f6ba16725ba225e92cda6523b0f594f5
Edgar Hurtado <edhurtado@utp.edu.co>
2025-04-25 18:57:21 -0500

    First Learning project in Java: Mockup FrontEnd + Backend for movies review   - Playing with basic primitive data types in Java

---
+++ myFirstJavaProject/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to ScreenMatch");
+        System.out.println("Movie: Matrix");
+
+        int fechaDeLanzamieto = 1999;
+        boolean includedInPlan = true;
+        
+    }
+}
